243fbc86a6d31455c3ad002db69d67d35f393cef
IanCode09 <ianlombu@protonmail.com>
2020-11-28 22:20:36 +0700

    Header Component Part 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,12 +25,12 @@ function Header() {
 
                 <div className="header__input">
                     <Search />
-                    <input type='text' />
+                    <input placeholder="Search Facebook" type='text' />
                 </div>
             </div>
 
             <div className="header__center">
-                <div className="header__option">
+                <div className="header__option header__option--active">
                     <Home fontSize='large' />
                 </div>
                 <div className="header__option">
