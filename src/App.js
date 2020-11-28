343b91380d1ba03f4fae68b011746c2adbf48e94
IanCode09 <ianlombu@protonmail.com>
2020-11-28 22:06:20 +0700

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="app">
-      <h1>Facebook Clone</h1>
+    
+      <Header />
+
+      {/* App body */}
+
+        {/* Sidebar */}
+
+        {/* Feed */}
+
+        {/* Widgets */}
+
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import '../css/Header.css'
+import { 
+    Search, 
+    Home, 
+    Flag, 
+    SubscriptionsOutlined, 
+    StorefrontOutlined, 
+    SupervisedUserCircle,
+    Add,
+    Forum,
+    NotificationsActive,
+    ExpandMore 
+} from '@material-ui/icons';
+import { Avatar, IconButton } from '@material-ui/core'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" 
+                    alt=""
+                />
+
+                <div className="header__input">
+                    <Search />
+                    <input type='text' />
+                </div>
+            </div>
+
+            <div className="header__center">
+                <div className="header__option">
+                    <Home fontSize='large' />
+                </div>
+                <div className="header__option">
+                    <Flag fontSize='large' />
+                </div>
+                <div className="header__option">
+                    <SubscriptionsOutlined fontSize='large' />
+                </div>
+                <div className="header__option">
+                    <StorefrontOutlined fontSize='large' />
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircle fontSize='large' />
+                </div>
+            </div>
+
+            <div className="header__right">
+                <div className="header__info">
+                    <Avatar />
+                    <h4>Ian Lombu</h4>
+                </div>
+
+                <IconButton>
+                    <Add />
+                </IconButton>
+
+                <IconButton>
+                    <Forum />
+                </IconButton>
+
+                <IconButton>
+                    <NotificationsActive />
+                </IconButton>
+
+                <IconButton>
+                    <ExpandMore />
+                </IconButton>
+            </div>
+        </div>
+    )
+}
+
+export default Header
