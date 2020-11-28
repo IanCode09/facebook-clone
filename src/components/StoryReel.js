fbd7aae24d151e9a05d30aad3c5f198a8e621ed0
IanCode09 <ianlombu@protonmail.com>
2020-11-29 00:42:23 +0700

    Styling Story Component

--- src/components/StoryReel.js
+++ src/components/StoryReel.js
@@ -3,29 +3,28 @@ import '../css/StoryReel.css'
 import Story from './Story'
 
 function StoryReel() {
-    const image = "https://images.unsplash.com/photo-1555066931-bf19f8fd1085?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1502&q=80"
     return (
         <div className="storyReel">
             <Story
-                image = {image}
+                image = "https://images.unsplash.com/flagged/photo-1563536310477-c7b4e3a800c2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1534&q=80"
                 profileSrc="https://avatars0.githubusercontent.com/u/69780199?s=460&u=3940fd45951e75044f959991d0107ee212d42207&v=4"
                 title="Ian Lombu"
             />
 
             <Story
-                image = {image}
+                image = "https://images.unsplash.com/photo-1544256718-3bcf237f3974?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1502&q=80"
                 profileSrc="https://avatars1.githubusercontent.com/u/8461930?s=460&v=4"
                 title="Qazi"
             />
 
             <Story
-                image = {image}
+                image = "https://images.unsplash.com/photo-1600718096300-5e9fc80ff942?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1534&q=80"
                 profileSrc="https://avatars2.githubusercontent.com/u/24712956?s=460&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
                 title="Sony"
             />
 
             <Story
-                image = {image}
+                image = "https://images.unsplash.com/photo-1588171372033-a42dd7cf8414?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1400&q=80"
                 profileSrc="https://avatars0.githubusercontent.com/u/6589966?s=460&u=92735e6e0182d3000728073fbaa438160e11ed90&v=4"
                 title="Nazariy Dumanskyy"
             />
